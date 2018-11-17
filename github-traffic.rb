bae1fec39ac75ba4c2594dc62cac4514395976d9
Alex Rocha <alex.rochas@yahoo.com.br>
2018-11-17 12:38:57 -0200

    feat: extract top 10 repos

---
+++ github-traffic.rb
@@ -0,0 +1,21 @@
+
+require 'octokit'
+require 'pp'
+
+client = Octokit::Client.new(:login => 'alexrochas', :password => ENV['GITHUB_PASS'], per_page: 200)
+# client.auto_paginate = true
+
+pp client.repos
+repos = client
+          .repos
+          .select {|repo| !repo.private }
+          .map {|repo| repo.full_name }
+          .map { |repo| {:repo => repo, :views => client.views(repo, {accept: "application/vnd.github.squirrel-girl-preview+json"})} }
+
+repos.size
+
+repos.sort_by { |repo| 
+    -repo[:views][:uniques]
+  }.take(10).each_with_index { |repo, index| 
+    puts "\##{index+1} - #{repo[:repo]} with #{repo[:views][:uniques]} unique views"
+  }
